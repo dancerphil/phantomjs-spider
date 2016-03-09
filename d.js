415c14bbef6c2b7138bf40b067f1888c070f5cc2
dancerphil <413149044@qq.com>
2016-03-09 16:37:00 +0800

    temp

--- d.js
+++ d.js
@@ -5,41 +5,25 @@ var urls=file.read();
 file.close();
 urlArray=urls.split('\r\n');
 
-var p=[];
-for (i in urlArray){
-	p[i]=require('webpage').create();
-}
+var p=new Array(urlArray.length);
+console.log(p.length);
 
+i=0
+p[i] = require('webpage').create();
+p[i].open(urlArray[i], function(status) {
+	console.log("Status: " + status);
+	  if(status === "success") {
+		p[i].viewportSize = { width: 58, height : 58 };
+		p[i].render(i+'.jpg');
+	}
+});
 
-var p1 = require('webpage').create();
-p1.open(urlArray[0], function(status) {
+i=1
+p[i] = require('webpage').create();
+p[i].open(urlArray[i], function(status) {
 	console.log("Status: " + status);
 	  if(status === "success") {
-		p1.viewportSize = { width: 58, height : 58 };
-		p1.render('0.jpg');
-	}
-});
-var p2 = require('webpage').create();
-p2.open(urlArray[1], function(status) {
-	console.log("Status: " + status);
-	  if(status === "success") {
-		p2.viewportSize = { width: 58, height : 58 };
-		p2.render('1.jpg');
-	}
-});
-var p3 = require('webpage').create();
-p3.open(urlArray[2], function(status) {
-	console.log("Status: " + status);
-	  if(status === "success") {
-		p3.viewportSize = { width: 58, height : 58 };
-		p3.render('2.jpg');
-	}
-});
-var p4 = require('webpage').create();
-p4.open(urlArray[3], function(status) {
-	console.log("Status: " + status);
-	  if(status === "success") {
-		p4.viewportSize = { width: 58, height : 58 };
-		p4.render('3.jpg');
+		p[i].viewportSize = { width: 58, height : 58 };
+		p[i].render(i+'.jpg');
 	}
 });
