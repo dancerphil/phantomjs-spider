de64edc16a91d096d9326d00003b9ecdc8f2f302
dancerphil <413149044@qq.com>
2016-03-11 12:08:46 +0800

    准备改个麻烦东西

--- UrlToImg.js
+++ UrlToImg.js
@@ -1,5 +1,5 @@
-// var country='德国'
-var file = require('fs').open("urls.txt", 'r')
+var country='德国'
+var file = require('fs').open(country+".txt", 'r')
 var urls = file.read()
 file.close()
 
@@ -7,17 +7,17 @@ urlArray = urls.split('\r\n')
 
 var pages = new Array(urlArray.length)
 
-for (var i = 0; i < pages.length; i++) {
+for (var i = 0; i < pages.length; i+=2) {
 	var t = function(i) {
 		pages[i] = require('webpage').create()
-		pages[i].open(urlArray[i], function(status) {
+		pages[i].open(urlArray[i+1], function(status) {
 			console.log("Status: " + status)
 			if (status === "success") {
 				pages[i].viewportSize = {
 					width: 58,
 					height: 58
 				}
-				pages[i].render(i + '.jpg')
+				pages[i].render(country+'/'+urlArray[i] + '.jpg')
 			}
 		})
 	}(i)
