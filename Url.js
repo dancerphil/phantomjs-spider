d7c1deccc7bf802e2121335ba4fe17f37717ba4d
dancerphil <413149044@qq.com>
2016-03-11 14:01:47 +0800

    暂时不改了

--- Url.js
+++ Url.js
@@ -1,22 +1,21 @@
+// 张聪 413149044@qq.com
 phantom.outputEncoding = "gbk";
-
-"use strict";
+var country = '法国'
+var countryNum = 4
 var fs = require('fs')
 var page = require('webpage').create();
+var outputString = ''
 
 var url = 'https://www.baidu.com/s?wd=%E6%B1%BD%E8%BD%A6%E5%93%81%E7%89%8C%E5%A4%A7%E5%85%A8&usm=1&ie=utf-8&rsv_cq=%E6%B1%BD%E8%BD%A6%E5%93%81%E7%89%8Clogo&rsv_dl=0_left_sp_img_4_18696'
 page.open(url, function(status) {
-	var outputString = ''
-	page.evaluate(function() {
-		$(".op_definitive_answer_po_tag_item")[0].click() //点击地区: 全部 德国 意大利 英国 法国 美国 中国 日本 其它
-	})
+	page.evaluate(clickCountry,countryNum) //点击地区: 全部 德国 意大利 英国 法国 美国 中国 日本 其它
 	setInterval(function() {
-		addToOutput(page.evaluate(readPage))
-		if (!page.evaluate(clickNext)) {
+		addToOutput(page.evaluate(readPage)) // 返回此页面上的图URL
+		if (!page.evaluate(clickNext)) { // 如果有下一页按钮，点击并跳过，如果已经是最后一页，输出并退出
 			writeFile()
 			phantom.exit()
 		}
-	}, 5000)
+	}, 5000) // 两秒太少，改进可以参考： https://github.com/BigMaster/hahou/tree/master/phantomjs-script/lib
 
 	function readPage() {
 		var urls = []
@@ -27,6 +26,10 @@ page.open(url, function(status) {
 			urls.push(imgs[i].src)
 		}
 		return urls
+	}
+
+	function clickCountry(countryNum) {
+		$(".op_definitive_answer_po_tag_item")[countryNum].click()
 	}
 
 	function clickNext() {
@@ -48,7 +51,7 @@ page.open(url, function(status) {
 	function writeFile() {
 		try {
 			//console.log(outputString)
-			fs.write('urls.txt', outputString, 'w');
+			fs.write(country + '.txt', outputString, 'w');
 		} catch (e) {
 			console.log(e);
 		}
--- UrlToImg.js
+++ UrlToImg.js
@@ -1,21 +1,22 @@
-var country='法国'
-var file = require('fs').open(country+".txt", 'r')
+// 张聪 413149044@qq.com
+var country = '法国'
+var file = require('fs').open(country + ".txt", 'r')
 var urls = file.read()
 file.close()
 
 urlArray = urls.split('\r\n')
 
-for (var i = 0; i < urlArray.length; i+=2) {// 两个一组：title url
+for (var i = 0; i < urlArray.length; i += 2) { // 两个一组：title url
 	var t = function(i) {
 		var page = require('webpage').create()
-		page.open(urlArray[i+1], function(status) {
+		page.open(urlArray[i + 1], function(status) {
 			console.log("Status: " + status)
 			if (status === "success") {
 				page.viewportSize = {
 					width: 58,
 					height: 58
 				}
-				page.render(country+'/'+urlArray[i] + '.jpg')
+				page.render(country + '/' + urlArray[i] + '.jpg')
 			}
 		})
 	}(i)
